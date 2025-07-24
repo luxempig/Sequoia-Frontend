edb99e463b64ffb2e3ac6602e001faec1ad94d70
Daniel Freymann <ddfreymann@gmail.com>
2025-07-23 23:08:29 -0400

    inital sequoia frontend

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,17 @@
+// src/App.tsx
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import VoyageList   from './components/VoyageList';    // your pre-auth list
+import VoyageDetail from './components/VoyageDetail';  // your detail view
+import './index.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App: React.FC = () => (
+  <BrowserRouter>
+    <Routes>
+      <Route path="/"          element={<VoyageList   />} />
+      <Route path="/voyages/:id" element={<VoyageDetail />} />
+    </Routes>
+  </BrowserRouter>
+);
 
 export default App;
