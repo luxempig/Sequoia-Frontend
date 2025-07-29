9976275ae640c7cc077347298acd84577fef2545
Daniel <daniel@Daniels-MacBook-Air.local>
2025-07-29 16:23:51 -0400

    fixed app.tsx for the purpose of buttons working

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,22 @@
+// File: src/App.tsx
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 import HomePage     from './components/HomePage';
+import VoyageList   from './components/VoyageList';
 import VoyageDetail from './components/VoyageDetail';
 import './index.css';
 
 const App: React.FC = () => (
   <BrowserRouter>
     <Routes>
-      <Route path="/"            element={<HomePage />} />
+      {/* Landing page */}
+      <Route path="/" element={<HomePage />} />
+
+      {/* Full voyage timeline list */}
+      <Route path="/voyages" element={<VoyageList />} />
+
+      {/* Individual voyage detail */}
       <Route path="/voyages/:id" element={<VoyageDetail />} />
     </Routes>
   </BrowserRouter>
