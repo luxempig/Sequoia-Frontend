9cb123293ec19c8a70d12953e40fd5f6ae79314e
Daniel Freymann <ddfreymann@gmail.com>
2025-08-13 04:50:18 -0400

    app.tsx changes for refactor

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,23 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import HomePage     from './components/HomePage';
-import VoyageList   from './components/VoyageList';
-import VoyageDetail from './components/VoyageDetail';
-import './index.css';
+import HomePage from "./components/HomePage";
+import VoyageList from "./components/VoyageList";
+import VoyageDetail from "./components/VoyageDetail";
+import PresidentDirectory from "./components/PresidentDirectory";
+import PresidentVoyages from "./components/PresidentVoyages";
+import "./index.css";
 
 const App: React.FC = () => (
   <BrowserRouter>
     <Routes>
-      {/* Landing page */}
       <Route path="/" element={<HomePage />} />
-
-      {/* Full voyage timeline list */}
       <Route path="/voyages" element={<VoyageList />} />
-
-      {/* Individual voyage detail */}
       <Route path="/voyages/:id" element={<VoyageDetail />} />
+
+      {/* NEW: presidents directory + detail */}
+      <Route path="/presidents" element={<PresidentDirectory />} />
+      <Route path="/presidents/:id" element={<PresidentVoyages />} />
     </Routes>
   </BrowserRouter>
 );
